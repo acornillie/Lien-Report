1d349d018b9d2adb1287f2b5b90eac31d2dc65ca
josephenglund <46461687+josephenglund@users.noreply.github.com>
2019-01-09 08:26:58 -0600

    Update LIEN REPORT#2 (v4).sql

--- LIEN REPORT#2 (v4).sql
+++ LIEN REPORT#2 (v4).sql
@@ -1,7 +1,7 @@
 use nineced
 
 DECLARE @CreditOffice int
-SET @CreditOffice = 9846										--Should be parameter in final report
+SET @CreditOffice = 9833										--Should be parameter in final report
 
 
 DECLARE @LastShip TABLE 
@@ -21,7 +21,7 @@ CA.id
 	left join ArTranExt ate on ate.id = ar.id
 	WHERE ar.custAccountId = ca.id 
 	and ate.shipDate is Not null
-	and ar.tranTypeId = 10
+	and ar.tranTypeId = 10										--TranTypeId 10 = Invoice
 	and ar.origAmount > 0										--Should be parameter in final report
 	ORDER BY ate.shipDate desc, ar.origAmount desc
 	)
@@ -31,36 +31,38 @@ LEFT OUTER JOIN OrgMember om on ca.id = om.id
 WHERE 
 om.divisionId = @CreditOffice
 and EXISTS (SELECT * FROM ArTran atx WHERE atx.custAccountId = ca.id and atx.state = 0) 
-and ca.typeId = 101												--Need to confirm with Ben Graham that this is the only type this report needs to show
+and ca.typeId = 101												--Need to confirm with Ben Graham that this is the only type this report needs to show - TypeId 101 = Secured Lien
 
 select
 	c.[name]		as	'CustomerName'
 ,	c.num			as	'CustomerNumber'
 ,	ca.[name]		as	'CustomerAccountName'
 ,	ca.num			as	'CustomerAccountNumber'
-,	om.divisionid	as	'CreditOffice'							
+,	om.divisionid		as	'CreditOffice'							
 ,	om.branchid		as	'ProfitCenter'								
 ,	[at].refnum		as	'ReferenceNumber'							
-,	[at].docType	as	'DocumentType'								
+,	[at].docType		as	'DocumentType'								
 ,	l.[state]		as	'LienState'
 ,	l.jobName		as	'JobName'
-,	l.materialAmt	as	'MaterialAmount'
-,	oc.totalDue		as	'AmountPastDue'
-,	ate.shipDate	as	'LastShip'
-,	CONVERT (varchar, getdate(), 101)		as 'CurrentDate'
+,	l.materialAmt		as	'MaterialAmount'
+,	oc.totalDue		as	'AmountDue'
+,	[at].origAmount 	as	'TotalAmountLastShip'	
+,	ate.shipDate		as	'LastShip'	
+,	[at].tranDate		as	'TransactionDate'	
+,	CONVERT (varchar, getdate(), 101)	as 'CurrentDate'
 ,	DATEDIFF (d, ate.shipDate, getdate())	as 'DayDifference'
-,	[at].tranDate	as	'TransactionDate'							
-,	[at].origAmount as	'TotalAmount'							
+						
+					
 from CustAccount ca												--customeraccountname; customeraccountnumber
 left join Customer c			on c.id = ca.custid				--customername; customernumber
 left join CustAccountLien l		on ca.lienId = l.id				--lienstate; jobname; materialamount
 left join OrgCalc oc			on oc.orgId = ca.id				--totaldue
-left outer join @LastShip ls	on ca.id = ls.caID					
-left outer join ArTran [at]		on ls.lastshipid = [at].id		--referencenumber; documenttype
-left outer join ArTranExt ate	on [at].Id = ate.id				--lastship
-left outer join OrgMember om	on ca.id = om.id				--creditoffice; profitcenter
+left outer join @LastShip ls		on ca.id = ls.caID					
+left outer join ArTran [at]		on ls.lastshipid = [at].id			--referencenumber; documenttype
+left outer join ArTranExt ate		on [at].Id = ate.id				--lastship
+left outer join OrgMember om		on ca.id = om.id				--creditoffice; profitcenter
 WHERE 
 om.divisionId = @CreditOffice
 and oc.totaldue > 0												--Should be parameter in final report
-and DATEDIFF (d, ate.shipDate, getdate()) > 30					--Should be parameter in final report
-order by oc.totalDue
+and DATEDIFF (d, ate.shipDate, getdate()) > 1					--Should be parameter in final report
+order by 'TotalAmountLastShip'
